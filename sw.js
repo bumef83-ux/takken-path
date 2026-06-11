1bb831049af6c43ea0aa663f1ce7cc1ae8543260
Kensuke.O <bumef83-ux@users.noreply.github.com>
2026-06-11 19:34:12 +0900

    Improve Android PWA installation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "takken-path-v1";
-const APP_ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
+const CACHE_NAME = "takken-path-v2";
+const APP_ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg", "icon-192.png", "icon-512.png", "icon-maskable-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
